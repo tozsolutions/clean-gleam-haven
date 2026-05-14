03f850b2cb935faa05af9ea128fe949ddcf4ad32
tozsolutions <ozkanaktikan@gmail.com>
2026-05-14 22:54:54 +0300

    Build and dev server config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  server: {
+    host: "127.0.0.1",
+    port: 5173,
+  },
   tanstackStart: {
     server: { entry: "src/server.ts" },
   },
